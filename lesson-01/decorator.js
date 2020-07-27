35e6e12e02e1d9417f7f4a07bdd0511e28532946
Larisa <larisavalgutova@gmail.com>
2020-07-27 22:05:04 +0300

    Задача 2

--- lesson-01/decorator.js
+++ lesson-01/decorator.js
@@ -1,19 +1,46 @@
-function pause(func, time) {
-  return function (n) {
-    setTimeout(function () {
-      console.log("Факториал " + n + " равен " + func(n));
-    }, time);
-    console.log(`Функция выполниться с задержкой в ${time} милисекунды!`);
+// Задача 1
+
+// function pause(func, time) {
+//   return function (n) {
+//     setTimeout(function () {
+//       console.log("Факториал " + n + " равен " + func(n));
+//     }, time);
+//     console.log(`Функция выполниться с задержкой в ${time} милисекунды!`);
+//   };
+// }
+
+// function factorial(n) {
+//   let f = 1;
+//   for (let i = 1; i <= n; i++) {
+//     f *= i;
+//   }
+//   return f;
+// }
+
+// let paused = pause(factorial, 2000);
+// paused(14);
+
+// Задача 2
+
+function returnOblect(func, ...keys) {
+  const obj = {};
+  let values = func();
+
+  for (let i = 0; i < keys.length; i++) {
+    obj[keys[i]] = values[i];
+  }
+
+  return function () {
+    return obj;
   };
 }
 
-function factorial(n) {
-  let f = 1;
-  for (let i = 1; i <= n; i++) {
-    f *= i;
-  }
-  return f;
+function arr() {
+  return [1, 2, 3];
 }
 
-let paused = pause(factorial, 2000);
-paused(14);
+let funcDecoreted = returnOblect(arr, "one", "two", "three");
+
+let r = funcDecoreted();
+
+console.log(r.one);
