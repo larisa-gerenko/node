9fc1ad4c0b5c139a6cef0ddb6d70abb230955565
Larisa <larisavalgutova@gmail.com>
2020-08-01 01:18:29 +0300

    Исправление задачи 2

--- lesson-02/homework/task2.js
+++ lesson-02/homework/task2.js
@@ -9,25 +9,25 @@ fs.readFile(filename, "utf8", (err, data) => {
   }
 
   let arr = data.split(" ");
-  let out1 = "";
-  let out2 = "";
+  let out1 = [];
+  let out2 = [];
 
   arr.forEach((element) => {
     if (element % 2 == 0) {
-      out1 += element + " ";
+      out1.push(element);
     }
 
-    out2 += element ** 3 + " ";
+    out2.push(element ** 3);
   });
 
-  fs.writeFile("out-1.txt", out1, (err) => {
+  fs.writeFile("out-1.txt", out1.join(" "), (err) => {
     if (err) {
       console.log("Could not find or open file for reading");
       return;
     }
   });
 
-  fs.writeFile("out-2.txt", out2, (err) => {
+  fs.writeFile("out-2.txt", out2.join(" "), (err) => {
     if (err) {
       console.log("Could not find or open file for reading");
       return;
